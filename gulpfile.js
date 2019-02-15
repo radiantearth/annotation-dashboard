8d904c912675f23060e3f77b38a7e6a663080039
Drew Bollinger <drew@developmentseed.org>
2019-02-15 15:36:03 -0500

    Use gulp-uglify-es

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,51 @@
-'use strict';
+'use strict'
 
-var fs = require('fs');
-var gulp = require('gulp');
-var cp = require('child_process');
-var $ = require('gulp-load-plugins')();
-var del = require('del');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
-var watchify = require('watchify');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var sourcemaps = require('gulp-sourcemaps');
-var log = require('fancy-log');
-var exit = require('gulp-exit');
-var rev = require('gulp-rev');
-var revReplace = require('gulp-rev-replace');
-var SassString = require('node-sass').types.String;
-var notifier = require('node-notifier');
-var historyApiFallback = require('connect-history-api-fallback');
+var fs = require('fs')
+var gulp = require('gulp')
+var $ = require('gulp-load-plugins')()
+var del = require('del')
+var browserSync = require('browser-sync')
+var reload = browserSync.reload
+var watchify = require('watchify')
+var browserify = require('browserify')
+var source = require('vinyl-source-stream')
+var buffer = require('vinyl-buffer')
+var sourcemaps = require('gulp-sourcemaps')
+var log = require('fancy-log')
+var exit = require('gulp-exit')
+var rev = require('gulp-rev')
+var revReplace = require('gulp-rev-replace')
+var SassString = require('node-sass').types.String
+var notifier = require('node-notifier')
+var historyApiFallback = require('connect-history-api-fallback')
+var uglify = require('gulp-uglify-es').default
 
 // /////////////////////////////////////////////////////////////////////////////
 // --------------------------- Variables -------------------------------------//
 // ---------------------------------------------------------------------------//
 
 // The package.json
-var pkg;
+var pkg
 
-// Environment
-// Set the correct environment, which controls what happens in config.js
-if (!process.env.DS_ENV) {
-  if (!process.env.CIRCLE_BRANCH || process.env.CIRCLE_BRANCH !== process.env.PRODUCTION_BRANCH) {
-    process.env.DS_ENV = 'staging';
-  } else {
-    process.env.DS_ENV = 'production';
-  }
-}
-
-var prodBuild = false;
+var prodBuild = false
 
 // /////////////////////////////////////////////////////////////////////////////
 // ------------------------- Helper functions --------------------------------//
 // ---------------------------------------------------------------------------//
 
 function readPackage () {
-  pkg = JSON.parse(fs.readFileSync('package.json'));
+  pkg = JSON.parse(fs.readFileSync('package.json'))
 }
-readPackage();
+readPackage()
 
 // /////////////////////////////////////////////////////////////////////////////
 // ------------------------- Callable tasks ----------------------------------//
 // ---------------------------------------------------------------------------//
 
 gulp.task('default', ['clean'], function () {
-  prodBuild = true;
-  gulp.start('build');
-});
+  prodBuild = true
+  gulp.start('build')
+})
 
 gulp.task('serve', ['vendorScripts', 'javascript', 'styles', 'fonts'], function () {
   browserSync({
@@ -69,24 +59,24 @@ gulp.task('serve', ['vendorScripts', 'javascript', 'styles', 'fonts'], function 
         historyApiFallback()
       ]
     }
-  });
+  })
 
   // watch for changes
   gulp.watch([
     'app/*.html',
     'app/assets/graphics/**/*'
-  ]).on('change', reload);
+  ]).on('change', reload)
 
-  gulp.watch('app/assets/styles/**/*.scss', ['styles']);
-  gulp.watch('package.json', ['vendorScripts']);
-});
+  gulp.watch('app/assets/styles/**/*.scss', ['styles'])
+  gulp.watch('package.json', ['vendorScripts'])
+})
 
 gulp.task('clean', function () {
   return del(['.tmp', 'dist'])
     .then(function () {
-      $.cache.clearAll();
-    });
-});
+      $.cache.clearAll()
+    })
+})
 
 // /////////////////////////////////////////////////////////////////////////////
 // ------------------------- Browserify tasks --------------------------------//
@@ -103,24 +93,24 @@ gulp.task('javascript', function () {
     cache: {},
     packageCache: {},
     fullPaths: true
-  }), {poll: true});
+  }), {poll: true})
 
   function bundler () {
     if (pkg.dependencies) {
-      watcher.external(Object.keys(pkg.dependencies));
+      watcher.external(Object.keys(pkg.dependencies))
     }
     return watcher.bundle()
       .on('error', function (e) {
         notifier.notify({
           title: 'Oops! Browserify errored:',
           message: e.message
-        });
-        console.log('Javascript error:', e);
+        })
+        console.log('Javascript error:', e)
         if (prodBuild) {
-          process.exit(1);
+          process.exit(1)
         }
         // Allows the watch to continue.
-        this.emit('end');
+        this.emit('end')
       })
       .pipe(source('bundle.js'))
       .pipe(buffer())
@@ -128,25 +118,25 @@ gulp.task('javascript', function () {
       .pipe(sourcemaps.init({loadMaps: true}))
       .pipe(sourcemaps.write('./'))
       .pipe(gulp.dest('.tmp/assets/scripts'))
-      .pipe(reload({stream: true}));
+      .pipe(reload({stream: true}))
   }
 
   watcher
     .on('log', log)
-    .on('update', bundler);
+    .on('update', bundler)
 
-  return bundler();
-});
+  return bundler()
+})
 
 // Vendor scripts. Basically all the dependencies in the package.js.
 // Therefore be careful and keep the dependencies clean.
 gulp.task('vendorScripts', function () {
   // Ensure package is updated.
-  readPackage();
+  readPackage()
   var vb = browserify({
     debug: true,
     require: pkg.dependencies ? Object.keys(pkg.dependencies) : []
-  });
+  })
   return vb.bundle()
     .on('error', log.bind(log, 'Browserify Error'))
     .pipe(source('vendor.js'))
@@ -154,8 +144,8 @@ gulp.task('vendorScripts', function () {
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('.tmp/assets/scripts/'))
-    .pipe(reload({stream: true}));
-});
+    .pipe(reload({stream: true}))
+})
 
 // //////////////////////////////////////////////////////////////////////////////
 // --------------------------- Helper tasks -----------------------------------//
@@ -165,9 +155,9 @@ gulp.task('build', ['vendorScripts', 'javascript', 'fonts'], function () {
   gulp.start(['html', 'images', 'extras'], function () {
     return gulp.src('dist/**/*')
       .pipe($.size({title: 'build', gzip: true}))
-      .pipe(exit());
-  });
-});
+      .pipe(exit())
+  })
+})
 
 gulp.task('styles', function () {
   return gulp.src('app/assets/styles/main.scss')
@@ -175,13 +165,13 @@ gulp.task('styles', function () {
       notifier.notify({
         title: 'Oops! Sass errored:',
         message: e.message
-      });
-      console.log('Sass error:', e.toString());
+      })
+      console.log('Sass error:', e.toString())
       if (prodBuild) {
-        process.exit(1);
+        process.exit(1)
       }
       // Allows the watch to continue.
-      this.emit('end');
+      this.emit('end')
     }))
     .pipe($.sourcemaps.init())
     .pipe($.sass({
@@ -189,29 +179,27 @@ gulp.task('styles', function () {
       precision: 10,
       functions: {
         'urlencode($url)': function (url) {
-          var v = new SassString();
-          v.setValue(encodeURIComponent(url.getValue()));
-          return v;
+          var v = new SassString()
+          v.setValue(encodeURIComponent(url.getValue()))
+          return v
         }
       },
       includePaths: require('node-bourbon').with('node_modules/jeet')
     }))
     .pipe($.sourcemaps.write())
     .pipe(gulp.dest('.tmp/assets/styles'))
-    .pipe(reload({stream: true}));
-});
+    .pipe(reload({stream: true}))
+})
 
 gulp.task('html', ['styles'], function () {
   return gulp.src('app/*.html')
     .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
-    // Do not compress comparisons, to avoid MapboxGLJS minification issue
-    // https://github.com/mapbox/mapbox-gl-js/issues/4359#issuecomment-286277540
-    // .pipe($.if('*.js', $.uglify({compress: {comparisons: false}})))
+    .pipe($.if('*.js', uglify()))
     .pipe($.if('*.css', $.csso()))
     .pipe($.if(/\.(css|js)$/, rev()))
     .pipe(revReplace())
-    .pipe(gulp.dest('dist'));
-});
+    .pipe(gulp.dest('dist'))
+})
 
 gulp.task('images', function () {
   return gulp.src('app/assets/graphics/**/*')
@@ -223,8 +211,8 @@ gulp.task('images', function () {
       // as hooks for embedding and styling
       $.imagemin.svgo({plugins: [{cleanupIDs: false}]})
     ])))
-    .pipe(gulp.dest('dist/assets/graphics'));
-});
+    .pipe(gulp.dest('dist/assets/graphics'))
+})
 
 gulp.task('extras', function () {
   return gulp.src([
@@ -237,11 +225,11 @@ gulp.task('extras', function () {
     '!app/assets/scripts/**'
   ], {
     dot: true
-  }).pipe(gulp.dest('dist'));
-});
+  }).pipe(gulp.dest('dist'))
+})
 
 gulp.task('fonts', function () {
   return gulp.src('app/assets/fonts/**/*')
     .pipe(gulp.dest('.tmp/assets/fonts'))
-    .pipe(gulp.dest('dist/assets/fonts'));
-});
+    .pipe(gulp.dest('dist/assets/fonts'))
+})
