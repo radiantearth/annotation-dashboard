9262022b047e08549829b70bb159bfd3cfa5d430
Drew Bollinger <drew@developmentseed.org>
2019-02-15 10:00:07 -0500

    Add production config

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -1,5 +1,12 @@
 'use strict'
 
 export default {
-  environment: 'production'
+  environment: 'production',
+  mbToken: 'pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJnUi1mbkVvIn0.018aLhX0Mb0tdtaT2QNe2Q',
+  appName: 'Label Validation Tool',
+  appDescription: 'Webtool for validating satellite imagery machine learning labels',
+  api: 'https://api.radiant.earth/platform',
+  AUTH0_CLIENT_ID: 'Tbd4PFIt2PxBQBga7EKO3vfwRcr5rejI',
+  AUTH0_DOMAIN: 'raster-foundry.auth0.com',
+  CATALOG_API: 'https://p7815oz5la.execute-api.us-west-2.amazonaws.com/Prod/add'
 }
